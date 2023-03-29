8935a1f8660855991b2aefac1231830838f87ef0
DrHIBOOSHKI <jirehpearson@outlook.com>
2023-03-30 11:16:29 +1300

    Update 01_yes_no_v3.py

--- 01_yes_no_v3.py
+++ 01_yes_no_v3.py
@@ -1,18 +1,21 @@
 #LU Yes/No
 
-# Ask the user if they have played before
-show_instructions = input("Have you played this game before?: ").lower()
+show_instructions = ""
+while show_instructions != "x":
 
-# If they say yes, output 'Program Continues'
-if show_instructions == "yes" or show_instructions == "y":
-    print("program continues")
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before?: ").lower()
 
-# If they say no, output 'Display Instructions'
-elif show_instructions == "no" or show_instructions == "n":
-    print("Display instructions")
+    # If they say yes, output 'Program Continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
 
-# Otherwise - show error
-else:
-    print("Please answer 'yes' or 'no'")
+    # If they say no, output 'Display Instructions'
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
 
-print(f"You entered '{show_instructions}'")
+    # Otherwise - show error
+    else:
+        print("Please answer 'yes' or 'no'")
+
+    print(f"You entered '{show_instructions}'")
